e9079d3521c209859937e593da29d0af535d82b9
jiangyuanshu <317787106@qq.com>
2022-09-13 17:50:00 +0800

    support spring boot and slf4j

--- src/main/java/org/tron/p2p/P2pService.java
+++ src/main/java/org/tron/p2p/P2pService.java
@@ -1,4 +1,17 @@
 package org.tron.p2p;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
+@Slf4j
+@EnableScheduling
+// @RestController
 public class P2pService {
+  public static void main(String[] args) {
+    SpringApplication.run(P2pService.class, args);
+  }
 }
