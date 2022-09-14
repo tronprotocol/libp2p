71690846f06af75e9e5845028daa63d9f7352290
jiangyuanshu <317787106@qq.com>
2022-09-14 15:06:44 +0800

    add config file application.yml

--- src/main/java/org/tron/p2p/P2pConfig.java
+++ src/main/java/org/tron/p2p/P2pConfig.java
@@ -1,4 +1,17 @@
 package org.tron.p2p;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationProperties("netconfig")
+@Data
+@Configuration
 public class P2pConfig {
+  @Value("${netconfig.tcp_port:1001}")
+  private int tcpPort;
+
+  @Value("${netconfig.udp_port:1002}")
+  private int udpPort;
 }
--- src/main/java/org/tron/p2p/discover/socket/DiscoverServer.java
+++ src/main/java/org/tron/p2p/discover/socket/DiscoverServer.java
@@ -1,4 +1,10 @@
 package org.tron.p2p.discover.socket;
 
+import org.springframework.scheduling.annotation.Scheduled;
+
 public class DiscoverServer {
+
+  @Scheduled(cron = "${task.cron}")
+  public void testCron() {
+  }
 }
