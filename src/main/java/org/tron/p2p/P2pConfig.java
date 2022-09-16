032047628260db1dca236e7c2b2f927c6febd447
jiangyuanshu <317787106@qq.com>
2022-09-16 16:37:05 +0800

    update P2pConfig

--- src/main/java/org/tron/p2p/P2pConfig.java
+++ src/main/java/org/tron/p2p/P2pConfig.java
@@ -5,46 +5,69 @@ import java.util.List;
 
 public class P2pConfig {
 
-  private final List<InetSocketAddress> seedNodes;
-  private final List<InetSocketAddress> activeNodes;
-  private final List<InetSocketAddress> trustNodes;
+  private List<InetSocketAddress> seedNodes;
+  private List<InetSocketAddress> activeNodes;
+  private List<InetSocketAddress> trustNodes;
   private final byte[] nodeID;
   private final String ip;
-  private final int port;
+  private final int port; //udp port and tcp port
   private final int version;
-  private final int maxConnections;
-  private final int minConnections;
-  private final int minActiveConnections;
-  private final int maxConnectionsWithSameIp;
-  private final boolean discoverEnable;
-  private final boolean disconnectionPolicyEnable;
+  private int minConnections = 10;
+  private int maxConnections = 50;
+  private int minActiveConnections = 6;
+  private int maxConnectionsWithSameIp = 2;
+  private boolean discoverEnable = false;
+  private boolean disconnectionPolicyEnable = false;
 
-  public P2pConfig(List<InetSocketAddress> seedNodes,
-      List<InetSocketAddress> activeNodes,
-      List<InetSocketAddress> trustNodes,
-      byte[] nodeID,
-      String ip,
-      int port,
-      int version,
-      int maxConnections,
-      int minConnections,
-      int minActiveConnections,
-      int maxConnectionsWithSameIp,
-      boolean discoverEnable,
-      boolean disconnectionPolicyEnable) {
-    this.seedNodes = seedNodes;
-    this.activeNodes = activeNodes;
-    this.trustNodes = trustNodes;
+  public P2pConfig(byte[] nodeID, String ip, int port, int version) {
     this.nodeID = nodeID;
     this.ip = ip;
     this.port = port;
     this.version = version;
-    this.maxConnections = maxConnections;
-    this.minConnections = minConnections;
-    this.minActiveConnections = minActiveConnections;
-    this.maxConnectionsWithSameIp = maxConnectionsWithSameIp;
-    this.discoverEnable = discoverEnable;
-    this.disconnectionPolicyEnable = disconnectionPolicyEnable;
   }
 
+  public P2pConfig withSeedNodes(List<InetSocketAddress> seedNodes) {
+    this.seedNodes = seedNodes;
+    return this;
+  }
+
+  public P2pConfig withTrustNodes(List<InetSocketAddress> trustNodes) {
+    this.trustNodes = trustNodes;
+    return this;
+  }
+
+  public P2pConfig withActiveNodes(List<InetSocketAddress> activeNodes) {
+    this.activeNodes = activeNodes;
+    return this;
+  }
+
+  public P2pConfig withMinConnections(int minConnections) {
+    this.minConnections = minConnections;
+    return this;
+  }
+
+  public P2pConfig withMaxConnections(int maxConnections) {
+    this.maxConnections = maxConnections;
+    return this;
+  }
+
+  public P2pConfig withMinActiveConnections(int minActiveConnections) {
+    this.minActiveConnections = minActiveConnections;
+    return this;
+  }
+
+  public P2pConfig withMaxConnectionsWithSameIp(int maxConnectionsWithSameIp) {
+    this.maxConnectionsWithSameIp = maxConnectionsWithSameIp;
+    return this;
+  }
+
+  public P2pConfig withDiscoverEnable(boolean discoverEnable) {
+    this.discoverEnable = discoverEnable;
+    return this;
+  }
+
+  public P2pConfig withDisconnectionPolicyEnable(boolean disconnectionPolicyEnable) {
+    this.disconnectionPolicyEnable = disconnectionPolicyEnable;
+    return this;
+  }
 }
