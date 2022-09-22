fe6a05914bfb2e82a97a8cfb53a263474d22f784
wubin01 <wb_bupt@163.com>
2022-09-22 14:15:34 +0800

    add get ip interface

--- src/main/java/org/tron/p2p/P2pConfig.java
+++ src/main/java/org/tron/p2p/P2pConfig.java
@@ -3,101 +3,24 @@ package org.tron.p2p;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.Getter;
+
+import lombok.Data;
 import org.tron.p2p.discover.Node;
+import org.tron.p2p.utils.NetUtil;
 
+@Data
 public class P2pConfig {
-
-  @Getter
   private List<InetSocketAddress> seedNodes = new ArrayList<>();
-  @Getter
   private List<InetSocketAddress> activeNodes = new ArrayList<>();
-  @Getter
   private List<InetSocketAddress> trustNodes = new ArrayList<>();
-  @Getter
-  private final byte[] nodeID;
-  @Getter
-  private final String ip;
-  @Getter
-  private int port = 18888; //udp port and tcp port
-  @Getter
-  private final int version;
-  @Getter
-  private int minConnections = 10;
-  @Getter
+  private byte[] nodeID = Node.getNodeId();
+  private String ip = NetUtil.getExternalIp();
+  private int port = 18888;
+  private int version = 1;
+  private int minConnections = 8;
   private int maxConnections = 50;
-  @Getter
-  private int minActiveConnections = 6;
-  @Getter
+  private int minActiveConnections = 2;
   private int maxConnectionsWithSameIp = 2;
-  @Getter
-  private boolean discoverEnable = false;
-  @Getter
+  private boolean discoverEnable = true;
   private boolean disconnectionPolicyEnable = false;
-  @Getter
-  private int discoveryPingTimeOut = 15000;
-
-  public P2pConfig(String ip, int port, int version) {
-    this.nodeID = Node.getNodeId();
-    this.ip = ip;
-    this.port = port;
-    this.version = version;
-  }
-
-  public P2pConfig(byte[] nodeID, String ip, int port, int version) {
-    this.nodeID = nodeID;
-    this.ip = ip;
-    this.port = port;
-    this.version = version;
-  }
-
-  public P2pConfig withSeedNodes(List<InetSocketAddress> seedNodes) {
-    this.seedNodes = seedNodes;
-    return this;
-  }
-
-  public P2pConfig withTrustNodes(List<InetSocketAddress> trustNodes) {
-    this.trustNodes = trustNodes;
-    return this;
-  }
-
-  public P2pConfig withActiveNodes(List<InetSocketAddress> activeNodes) {
-    this.activeNodes = activeNodes;
-    return this;
-  }
-
-  public P2pConfig withMinConnections(int minConnections) {
-    this.minConnections = minConnections;
-    return this;
-  }
-
-  public P2pConfig withMaxConnections(int maxConnections) {
-    this.maxConnections = maxConnections;
-    return this;
-  }
-
-  public P2pConfig withMinActiveConnections(int minActiveConnections) {
-    this.minActiveConnections = minActiveConnections;
-    return this;
-  }
-
-  public P2pConfig withMaxConnectionsWithSameIp(int maxConnectionsWithSameIp) {
-    this.maxConnectionsWithSameIp = maxConnectionsWithSameIp;
-    return this;
-  }
-
-  public P2pConfig withDiscoverEnable(boolean discoverEnable) {
-    this.discoverEnable = discoverEnable;
-    return this;
-  }
-
-  public P2pConfig withDisconnectionPolicyEnable(boolean disconnectionPolicyEnable) {
-    this.disconnectionPolicyEnable = disconnectionPolicyEnable;
-    return this;
-  }
-
-  public P2pConfig withDiscoveryPingTimeOut(int discoveryPingTimeOut) {
-    this.discoveryPingTimeOut = discoveryPingTimeOut;
-    return this;
-  }
 }
