5ae881900fc972dc34782df94c42d349b72b3b61
Brown Jiang <317787106@qq.com>
2023-09-12 11:45:16 +0800

    hotfix(net): don't submit new task if server is closing (#56)
    * hotfix(net): don't submit new task if server is closing

--- src/main/java/org/tron/p2p/connection/ChannelManager.java
+++ src/main/java/org/tron/p2p/connection/ChannelManager.java
@@ -57,6 +57,7 @@ public class ChannelManager {
       .newBuilder().maximumSize(2000).build(); //ban timestamp
 
   private static boolean isInit = false;
+  public static volatile boolean isShutdown = false;
 
   public static void init() {
     isInit = true;
@@ -155,9 +156,10 @@ public class ChannelManager {
   }
 
   public static void close() {
-    if (!isInit) {
+    if (!isInit || isShutdown) {
       return;
     }
+    isShutdown = true;
     connPoolService.close();
     keepAliveService.close();
     peerServer.close();
