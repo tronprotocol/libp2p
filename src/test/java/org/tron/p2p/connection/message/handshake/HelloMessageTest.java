b83c5dd9ec30f5ca68bb509430a34aad6d48ad64
Brown Jiang <317787106@qq.com>
2023-12-27 15:06:23 +0800

    feature: update dependency version (#74)
    * update dependency version

--- src/test/java/org/tron/p2p/connection/message/handshake/HelloMessageTest.java
+++ src/test/java/org/tron/p2p/connection/message/handshake/HelloMessageTest.java
@@ -1,13 +1,13 @@
 package org.tron.p2p.connection.message.handshake;
 
-import org.bouncycastle.pqc.math.linearalgebra.ByteUtils;
+import static org.tron.p2p.base.Parameter.p2pConfig;
+
+import java.util.Arrays;
 import org.junit.Assert;
 import org.junit.Test;
 import org.tron.p2p.P2pConfig;
 import org.tron.p2p.connection.business.handshake.DisconnectCode;
 import org.tron.p2p.connection.message.MessageType;
-
-import static org.tron.p2p.base.Parameter.p2pConfig;
 
 public class HelloMessageTest {
 
@@ -16,14 +16,15 @@ public class HelloMessageTest {
     p2pConfig = new P2pConfig();
     HelloMessage m1 = new HelloMessage(DisconnectCode.NORMAL);
     Assert.assertEquals(0, m1.getCode());
-    Assert.assertTrue(ByteUtils.equals(p2pConfig.getNodeID(), m1.getFrom().getId()));
+
+    Assert.assertTrue(Arrays.equals(p2pConfig.getNodeID(), m1.getFrom().getId()));
     Assert.assertEquals(p2pConfig.getPort(), m1.getFrom().getPort());
     Assert.assertEquals(p2pConfig.getIp(), m1.getFrom().getHostV4());
     Assert.assertEquals(p2pConfig.getNetworkId(), m1.getNetworkId());
     Assert.assertEquals(MessageType.HANDSHAKE_HELLO, m1.getType());
 
     HelloMessage m2 = new HelloMessage(m1.getData());
-    Assert.assertTrue(ByteUtils.equals(p2pConfig.getNodeID(), m2.getFrom().getId()));
+    Assert.assertTrue(Arrays.equals(p2pConfig.getNodeID(), m2.getFrom().getId()));
     Assert.assertEquals(p2pConfig.getPort(), m2.getFrom().getPort());
     Assert.assertEquals(p2pConfig.getIp(), m2.getFrom().getHostV4());
     Assert.assertEquals(p2pConfig.getNetworkId(), m2.getNetworkId());
