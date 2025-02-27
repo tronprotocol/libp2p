7da19c2a3dbb37359f8a006df04da061c01d8a9c
zeusoo001 <zeus_oo@163.com>
2025-02-27 18:00:22 +0800

    increase randomness when selecting TCP nodes

--- src/main/java/org/tron/p2p/connection/business/pool/ConnPoolService.java
+++ src/main/java/org/tron/p2p/connection/business/pool/ConnPoolService.java
@@ -58,6 +58,7 @@ public class ConnPoolService extends P2pEventHandler {
   public P2pConfig p2pConfig = Parameter.p2pConfig;
   private PeerClient peerClient;
   private List<InetSocketAddress> configActiveNodes = new ArrayList<>();
+  private int minCandidateSize = 100;
 
   public ConnPoolService() {
     this.messageTypes = new HashSet<>(); //no message type registers
@@ -214,6 +215,11 @@ public class ConnPoolService extends P2pEventHandler {
     }
 
     filtered.sort(Comparator.comparingLong(node -> -node.getUpdateTime()));
+    int candidateSize = Math.max(limit * 10, minCandidateSize);
+    if (filtered.size() > candidateSize) {
+      filtered = filtered.subList(0, candidateSize);
+    }
+    Collections.shuffle(filtered);
     return CollectionUtils.truncate(filtered, limit);
   }
 
