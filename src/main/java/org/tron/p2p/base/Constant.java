ac0565f0772810b1437710b24187dcf03e2bde5f
halibobo1205 <halibobo1205@gmail.com>
2025-03-11 11:42:39 +0800

    fix(base): fix get getExternalIpV4 from ifconfig.me

--- src/main/java/org/tron/p2p/base/Constant.java
+++ src/main/java/org/tron/p2p/base/Constant.java
@@ -7,7 +7,7 @@ public class Constant {
 
   public static final int NODE_ID_LEN = 64;
   public static final List<String> ipV4Urls = Arrays.asList(
-      "http://checkip.amazonaws.com", "https://ifconfig.me/", "https://4.ipw.cn/");
+      "http://checkip.amazonaws.com", "https://ifconfig.me/ip", "https://4.ipw.cn/");
   public static final List<String> ipV6Urls = Arrays.asList(
       "https://v6.ident.me", "http://6.ipw.cn/");
   public static final String ipV4Hex = "00000000"; //32 bit
